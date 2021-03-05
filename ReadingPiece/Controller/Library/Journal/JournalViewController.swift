40a50351d206abdd049512a3960049b57c0d8e9c
aawlgus <78068968+aawlgus@users.noreply.github.com>
2021-03-05 20:00:21 +0900

    feat: 일지 탭 테이블 뷰 추가
    테이블 뷰 셀에서 auto layout 추가 설정 필요. (self-sizing이 안 되는 문제)

--- ReadingPiece/Controller/Library/Journal/JournalViewController.swift
+++ ReadingPiece/Controller/Library/Journal/JournalViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class JournalViewController: UIViewController {
+    
+    let journalCell = JournalCell()
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "JournalCell", bundle: nil), forCellReuseIdentifier: journalCell.cellID)
+        //tableView.rowHeight = UITableView.automaticDimension
+        //tableView.estimatedRowHeight = 150
+        tableView.rowHeight = 150
+        
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = #colorLiteral(red: 0.9646214843, green: 0.9647600055, blue: 0.9645912051, alpha: 1)
     }
     
 
@@ -26,4 +37,27 @@ class JournalViewController: UIViewController {
     }
     */
 
+}
+
+extension JournalViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: journalCell.cellID) as! JournalCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = Bundle.main.loadNibNamed("JournalHeaderCell", owner: self, options: nil)?.first as! JournalHeaderCell
+        return headerView
+    }
+    
+    
 }
