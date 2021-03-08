178aad3464f4cad0745ab79fbeaf2df35bd5623d
aawlgus <78068968+jihyundev@users.noreply.github.com>
2021-03-08 11:22:15 +0900

    fix: merge 도중 발생한 오류 해결

--- ReadingPiece/Controller/Challenge/TimerViewController.swift
+++ ReadingPiece/Controller/Challenge/TimerViewController.swift
@@ -29,7 +29,7 @@ class TimerViewController: UIViewController {
         self.navigationItem.rightBarButtonItem?.tintColor = .main
         timerBackgroundView.layer.borderWidth = 5
         timerBackgroundView.layer.borderColor = UIColor.sub2.cgColor
-        timerBackgroundView.asCircle()
+        //timerBackgroundView.asCircle()
         currentTimeLabel.textColor = .charcoal
         bookTitleLabel.textColor = .black
         let attributedString = NSMutableAttributedString(string: "")
--- ReadingPiece/Extensions/Colors.swift
+++ ReadingPiece/Extensions/Colors.swift
@@ -84,6 +84,10 @@ extension UIColor {
   @nonobjc class var fillDisabled: UIColor {
     return UIColor(red: 244.0 / 255.0, green: 244.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
   }
+    
+    @nonobjc class var melon: UIColor {
+      return UIColor(red: 1.0, green: 108.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
+    }
 
 }
 
@@ -115,85 +119,4 @@ extension UIColor {
 extension CGColor {
     class var deepRed: CGColor { CGColor(red: 218, green: 37, blue: 123, alpha: 0)}
     class var rightOrange: CGColor { CGColor(red: 231, green: 92, blue: 91, alpha: 0)}
-}
-
-
-extension UIColor {
-
-  @nonobjc class var lightgrey2: UIColor {
-    return UIColor(white: 246.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var lightgrey1: UIColor {
-    return UIColor(white: 242.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var middlegrey2: UIColor {
-    return UIColor(white: 218.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var black: UIColor {
-    return UIColor(white: 0.0, alpha: 1.0)
-  }
-
-  @nonobjc class var darkgrey: UIColor {
-    return UIColor(white: 128.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var middlegrey1: UIColor {
-    return UIColor(white: 181.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var white: UIColor {
-    return UIColor(white: 1.0, alpha: 1.0)
-  }
-
-  @nonobjc class var charcoal: UIColor {
-    return UIColor(white: 101.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var sub2: UIColor {
-    return UIColor(red: 1.0, green: 178.0 / 255.0, blue: 171.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var sub1: UIColor {
-    return UIColor(red: 1.0, green: 135.0 / 255.0, blue: 123.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var hover: UIColor {
-    return UIColor(red: 235.0 / 255.0, green: 88.0 / 255.0, blue: 75.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var coral: UIColor {
-    return UIColor(red: 253.0 / 255.0, green: 111.0 / 255.0, blue: 113.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var yellowSub: UIColor {
-    return UIColor(red: 1.0, green: 221.0 / 255.0, blue: 44.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var mintPoint: UIColor {
-    return UIColor(red: 69.0 / 255.0, green: 1.0, blue: 152.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var yellowSub2: UIColor {
-    return UIColor(red: 1.0, green: 235.0 / 255.0, blue: 105.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var clock: UIColor {
-    return UIColor(red: 171.0 / 255.0, green: 105.0 / 255.0, blue: 1.0, alpha: 1.0)
-  }
-
-  @nonobjc class var grey: UIColor {
-    return UIColor(white: 171.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var fillDisabled: UIColor {
-    return UIColor(red: 244.0 / 255.0, green: 244.0 / 255.0, blue: 245.0 / 255.0, alpha: 1.0)
-  }
-
-  @nonobjc class var melon: UIColor {
-    return UIColor(red: 1.0, green: 108.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
-  }
-
 }
