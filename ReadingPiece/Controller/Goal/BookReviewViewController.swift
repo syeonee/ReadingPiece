387560e4e1dfd255179af1499a2a506195c012f8
syeonee <syeoneee326@gmail.com>
2021-03-09 04:53:40 +0900

    feat: 목표 설정 부분 리뷰 UI 구현

--- ReadingPiece/Controller/Goal/BookReviewViewController.swift
+++ ReadingPiece/Controller/Goal/BookReviewViewController.swift
@@ -1,38 +1,30 @@
 //
-//  BookDetailViewController.swift
+//  BookReviewViewController.swift
 //  ReadingPiece
 //
-//  Created by SYEON on 2021/03/08.
+//  Created by SYEON on 2021/03/09.
 //
 
 import UIKit
 
-class BookDetailViewController: UIViewController {
+class BookReviewViewController: UIViewController {
 
     @IBOutlet weak var reviewTableView: UITableView!
-    @IBOutlet weak var contentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         reviewTableView.delegate = self
         reviewTableView.dataSource = self
-        reviewTableView.separatorStyle = .none
         reviewTableView.register(UINib(nibName: "ReviewTableViewCell", bundle: nil), forCellReuseIdentifier: "bookReviewCell")
         reviewTableView.rowHeight = 189.5
-        
-      
-        
-    }
-    
-    @IBAction func moreReviewTapped(_ sender: Any) {
         
     }
     
 }
 
-extension BookDetailViewController: UITableViewDelegate, UITableViewDataSource {
+extension BookReviewViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
