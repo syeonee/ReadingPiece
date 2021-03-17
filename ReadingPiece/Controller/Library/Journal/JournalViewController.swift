729af57d4f0f4082dffcdaf718d43cd57a873816
aawlgus <78068968+jihyundev@users.noreply.github.com>
2021-03-17 12:37:59 +0900

    feat: 일지탭 테이블 뷰 placeholder 추가

--- ReadingPiece/Controller/Library/Journal/JournalViewController.swift
+++ ReadingPiece/Controller/Library/Journal/JournalViewController.swift
@@ -22,15 +22,6 @@ class JournalViewController: UIViewController {
     // 더보기 기능을 위한 0 또는 1 값을 저장하기 위한 Array
     var more: [Int] = []
     
-    // 기본 데이터 리스트는 최신순으로 자동으로 추가되어 있어야 함
-    var journals = [
-        Journal(bookTitle: "보건교사 안은영", content: "인증 1일차. 보건교사다 잽싸게 도망가자 ", date: Date(timeIntervalSinceNow: 86400*5), readingPercentage: 48, time: "1시간 10분"),
-        Journal(bookTitle: "아르센 벵거 자서전 My Life in Red and White", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", date: Date(timeIntervalSinceNow: 86400*4), readingPercentage: 60, time: "30분"),
-        Journal(bookTitle: "달러구트 꿈 백화점", content: "달러구트 꿈 백화점은 우리에게 또다른 세상을 보여준다. 우리가 전혀 알지 못했던 사실을 익숙하면서도", date: Date(timeIntervalSinceNow: 86400*3), readingPercentage: 10, time: "10시간 35분"),
-        Journal(bookTitle: "보건교사 안은영", content: "인증 1일차. 보건교사다 잽싸게 도망가자  인증 1일차. 보건교사다 잽싸게 도망가자", date: Calendar.current.date(byAdding: .day, value: 3, to: Date(timeIntervalSinceNow: 86400*2))!, readingPercentage: 48, time: "1시간 10분"),
-        Journal(bookTitle: "아르센 벵거 자서전 My Life in Red and White", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", date: Date(timeIntervalSinceNow: 86400), readingPercentage: 60, time: "30분"),
-        Journal(bookTitle: "달러구트 꿈 백화점", content: "달러구트 꿈 백화점은 우리에게 또다른 세상을 보여준다. 우리가 전혀 알지 못했던 사실을 익숙하면서도", date: Date(), readingPercentage: 10, time: "10시간 35분")
-    ]
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -48,34 +39,28 @@ class JournalViewController: UIViewController {
         tableView.backgroundColor = .lightgrey2
         
         // 더보기 값 배열 초기화
-        self.more = Array<Int>(repeating: 0, count: journals.count)
-            }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.more = Array<Int>(repeating: 0, count: Journal.dummyData.count)
     }
-    */
-
+    
 }
 
 extension JournalViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return journals.count
+        let count = Journal.dummyData.count
+        if count == 0 {
+            let message = "아직 인증이 없어요. \n매일 독서 시간과 소감을 기록하고 \n챌린지를 달성해요!"
+            self.setEmptyView(image: UIImage(named: "recordIcon")!, message: message)
+        }
+        return count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let length = self.journals[0].content.utf8.count
+        let length = Journal.dummyData[0].content.utf8.count
         
-        if self.journals[indexPath.row].content.utf8.count <= length {
+        if Journal.dummyData[indexPath.row].content.utf8.count <= length {
             let cell = tableView.dequeueReusableCell(withIdentifier: fullJournalCell.cellID) as! FullJournalCell
-            let journal = self.journals[indexPath.row]
+            let journal = Journal.dummyData[indexPath.row]
             cell.bookTitleLabel.text = journal.bookTitle
             cell.journalTextLabel.text = journal.content
             cell.dateLabel.text = dateFormatter.string(from: journal.date)
@@ -86,7 +71,7 @@ extension JournalViewController: UITableViewDataSource, UITableViewDelegate {
             return cell
         } else if more[indexPath.row] == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: journalCell.cellID) as! JournalCell
-            let journal = self.journals[indexPath.row]
+            let journal = Journal.dummyData[indexPath.row]
             cell.bookTitleLabel.text = journal.bookTitle
             cell.journalTextLabel.text = journal.content
             cell.dateLabel.text = dateFormatter.string(from: journal.date)
@@ -98,7 +83,7 @@ extension JournalViewController: UITableViewDataSource, UITableViewDelegate {
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: fullJournalCell.cellID) as! FullJournalCell
-            let journal = self.journals[indexPath.row]
+            let journal = Journal.dummyData[indexPath.row]
             cell.bookTitleLabel.text = journal.bookTitle
             cell.journalTextLabel.text = journal.content
             cell.dateLabel.text = dateFormatter.string(from: journal.date)
@@ -111,14 +96,23 @@ extension JournalViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let cell = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerView.identifier) as! JournalHeaderCell
-        cell.count.text = String(journals.count)
-        cell.recentDelegate = self
-        cell.oldDelegate = self
-        return cell
+        if Journal.dummyData.count == 0 {
+            return nil
+        } else {
+            let cell = tableView.dequeueReusableHeaderFooterView(withIdentifier: headerView.identifier) as! JournalHeaderCell
+            cell.count.text = String(Journal.dummyData.count)
+            cell.recentDelegate = self
+            cell.oldDelegate = self
+            return cell
+        }
+        
     }
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 45
+        if Journal.dummyData.count == 0 {
+            return 0
+        } else {
+            return 45
+        }
     }
     
     
@@ -156,7 +150,8 @@ extension JournalViewController: JournalEditDelegate, FullJournalEditDelegate {
         }
         let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
         let destructive = UIAlertAction(title: "삭제", style: .destructive) { (action) in
-            self.journals.remove(at: indexPath[1])
+            Journal.dummyData.remove(at: indexPath[1])
+            self.more.remove(at: indexPath[1])
             self.tableView.deleteRows(at: [IndexPath(row: indexPath[1], section: 0)], with: .left)
             self.tableView.reloadData()  // 섹션 헤더 reload 위해 사용
         }
@@ -172,12 +167,81 @@ extension JournalViewController: JournalEditDelegate, FullJournalEditDelegate {
 // 정렬 기능
 extension JournalViewController: JournalOldestDelegate, JournalLatestDelegate {
     func sortOldFirst() {
-        journals.sort(by: { $0.date < $1.date })
+        Journal.dummyData.sort(by: { $0.date < $1.date })
         tableView.reloadData()
     }
     
     func sortRecentFirst() {
-        journals.sort(by: { $0.date > $1.date })
+        Journal.dummyData.sort(by: { $0.date > $1.date })
         tableView.reloadData()
+    }
+}
+
+// 데이터가 없을 경우 표시되는 Placeholder
+extension JournalViewController {
+    // 테이블 뷰에 데이터가 없을 경우 표시되는 placeholder
+    func setEmptyView(image: UIImage, message: String) {
+        let emptyView = UIView(frame: CGRect(x: self.view.center.x, y: self.view.center.y, width: self.view.bounds.size.width, height: self.view.bounds.size.height))
+        let imageView = UIImageView()
+        let messageLabel = UILabel()
+        let button = UIButton()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        messageLabel.font = .NotoSans(.medium, size: 15)
+        messageLabel.textColor = .charcoal
+        button.titleLabel?.font = .NotoSans(.medium, size: 15)
+        button.makeRoundedButtnon("독서 시작하기", titleColor: .white, borderColor: UIColor.melon.cgColor, backgroundColor: .melon)
+        
+        emptyView.addSubview(imageView)
+        emptyView.addSubview(messageLabel)
+        emptyView.addSubview(button)
+        emptyView.backgroundColor = #colorLiteral(red: 0.9646214843, green: 0.9647600055, blue: 0.9645912051, alpha: 1)
+        
+        //imageView.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: emptyView.topAnchor, constant: 42).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 57).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 63).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 80).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -80).isActive = true
+        button.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 40).isActive = true
+        button.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 40).isActive = true
+        button.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        
+        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        
+        
+        imageView.image = image
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        
+        self.tableView.backgroundView = emptyView
+        self.tableView.separatorStyle = .none
+    }
+    
+    @objc func buttonAction (_ sender: UIButton!) {
+        print("독서 시작 - 메인 VC로 이동 후 타이머 VC로 이동해야 함")
+        
+        //let homeNavigationVC = MyNavViewController()
+        //let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "") as! MyNavViewController
+        
+        /*
+        let timerVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "timerVC") as! TimerViewController
+        //self.navigationController?.pushViewController(TimerVC, animated: true)
+        
+        let viewControllers = self.navigationController!.viewControllers
+        let newViewControllers = NSMutableArray()
+        
+        // preserve the root view controller
+        newViewControllers.add(viewControllers[0])
+        // add the new view controller
+        newViewControllers.add(timerVC)
+        self.navigationController?.setViewControllers(newViewControllers as! [UIViewController], animated: true)
+        */
     }
 }
