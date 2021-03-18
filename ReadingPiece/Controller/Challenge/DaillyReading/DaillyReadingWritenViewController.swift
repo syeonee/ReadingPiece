cc0e68ee66245a5d85450cee5b1f7a4425b99bc9
RobertHan96 <yshan4329@gmail.com>
2021-03-18 16:12:31 +0900

    feat: 일지 이미지 첨부/삭제

--- ReadingPiece/Controller/Challenge/DaillyReading/DaillyReadingWritenViewController.swift
+++ ReadingPiece/Controller/Challenge/DaillyReading/DaillyReadingWritenViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class DaillyReadingWritenViewController: UIViewController {
+    let cellId = ReviewImageCell.identifier
+    let picker = UIImagePickerController()
+    var pickedImage : UIImage?
+
     @IBOutlet weak var commentTextField: UITextField!
     @IBOutlet weak var bookInfoView: UIView!
     @IBOutlet weak var bookThumbnailImage: UIImageView!
@@ -19,8 +23,13 @@ class DaillyReadingWritenViewController: UIViewController {
     @IBOutlet weak var privatePostButton: UIButton!
     @IBOutlet weak var postDairyButton: UIButton!
     @IBOutlet weak var commentLengthLabel: UILabel!
-    @IBOutlet weak var reviewImageView: UIImageView!
     @IBOutlet weak var reviewImageHeight: NSLayoutConstraint!
+    @IBOutlet weak var reviewImageView: UIImageView!
+    @IBOutlet weak var reviewImagePopButton: UIButton! {
+        didSet {
+            reviewImagePopButton.isHidden = !reviewImagePopButton.isHidden
+        }
+    }
     
     // 구현 편의상 버튼으로 구현, 따로 연결된 Action은 없음
     // UI셋업만 진행하고, 연결된 로직은 없는 컴포넌트
@@ -32,43 +41,12 @@ class DaillyReadingWritenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        picker.delegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationItem.title = "독서 일지"
-    }
-
-    private func setupUI() {
-        setNavBar()
-        bookInfoView.layer.addBorder([.bottom], color: .middlegrey2, width: 0.5)
-        readingStatusButton.makeRoundedTagButtnon("읽는 중", titleColor: .middlegrey1, borderColor: UIColor.middlegrey1.cgColor, backgroundColor: .white)
-        totalReadingTimeButton.makeRoundedTagButtnon(" 00분", titleColor: .middlegrey1, borderColor: UIColor.lightgrey1.cgColor, backgroundColor: .lightgrey1)
-        postDairyButton.makeRoundedButtnon("완료", titleColor: .white, borderColor: UIColor.main.cgColor, backgroundColor: .main)
-        readingPageInputButton.makeSmallRoundedButtnon("00p", titleColor: .white, borderColor: UIColor.main.cgColor, backgroundColor: .main)
-        readingPercentInputButton.makeSmallRoundedButtnon("00%", titleColor: .main, borderColor: UIColor.main.cgColor, backgroundColor: .white)
-        publicPostButton.makeSmallRoundedButtnon("전체 공개", titleColor: .white, borderColor: UIColor.darkgrey.cgColor, backgroundColor: .darkgrey)
-        privatePostButton.makeSmallRoundedButtnon("나만 보기", titleColor: .darkgrey, borderColor: UIColor.darkgrey.cgColor, backgroundColor: .white)
-        bookTitleLabel.textColor = .darkgrey
-        bookAuthorLabel.textColor = .darkgrey
-        commentTextField.contentVerticalAlignment = .top
-        commentTextField.textColor = .charcoal
-        commentTextField.backgroundColor = .lightgrey1
-        commentLengthLabel.textColor = .darkgrey
-        
-    }
-    
-    private func setNavBar() {
-        self.navigationItem.title = "독서 일지"
-        self.navigationController?.navigationBar.topItem?.title = ""
-        let rightButton = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(postDiary(sender:)))
-        self.navigationItem.rightBarButtonItem = rightButton
-        self.navigationItem.rightBarButtonItem?.tintColor = .main
-        self.navigationController?.navigationBar.tintColor = .darkgrey
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
     }
     
     @objc func postDiary(sender: UIBarButtonItem) {
@@ -78,12 +56,14 @@ class DaillyReadingWritenViewController: UIViewController {
     }
     
     @IBAction func addImage(_ sender: UIButton) {
-        DispatchQueue.main.async {
-            self.reviewImageHeight.constant = 75
-            self.reviewImageView.image = UIImage(named: "AppIcon")
-        }
+        addImageAlertAction()
     }
     
+    @IBAction func popImage(_ sender: UIButton) {
+        reviewImageHeight.constant = 0
+        reviewImagePopButton.isHidden = true
+        pickedImage = nil
+    }
     @IBAction func pageSelect(_ sender: UIButton) {
         let inputReadingStatusVC = self.storyboard?.instantiateViewController(withIdentifier: InputReadingStatusPopupViewController.storyobardId)
         inputReadingStatusVC?.modalTransitionStyle = .crossDissolve
@@ -109,5 +89,90 @@ class DaillyReadingWritenViewController: UIViewController {
     @IBAction func postDiary(_ sender: UIButton) {
         print("pageSelect")
 
+    }
+
+    private func setupUI() {
+        setNavBar()
+        bookInfoView.layer.addBorder([.bottom], color: .middlegrey2, width: 0.5)
+        readingStatusButton.makeRoundedTagButtnon("읽는 중", titleColor: .middlegrey1, borderColor: UIColor.middlegrey1.cgColor, backgroundColor: .white)
+        totalReadingTimeButton.makeRoundedTagButtnon(" 00분", titleColor: .middlegrey1, borderColor: UIColor.lightgrey1.cgColor, backgroundColor: .lightgrey1)
+        postDairyButton.makeRoundedButtnon("완료", titleColor: .white, borderColor: UIColor.main.cgColor, backgroundColor: .main)
+        readingPageInputButton.makeSmallRoundedButtnon("00p", titleColor: .white, borderColor: UIColor.main.cgColor, backgroundColor: .main)
+        readingPercentInputButton.makeSmallRoundedButtnon("00%", titleColor: .main, borderColor: UIColor.main.cgColor, backgroundColor: .white)
+        publicPostButton.makeSmallRoundedButtnon("전체 공개", titleColor: .white, borderColor: UIColor.darkgrey.cgColor, backgroundColor: .darkgrey)
+        privatePostButton.makeSmallRoundedButtnon("나만 보기", titleColor: .darkgrey, borderColor: UIColor.darkgrey.cgColor, backgroundColor: .white)
+        reviewImagePopButton.isHidden = true
+        bookTitleLabel.textColor = .darkgrey
+        bookAuthorLabel.textColor = .darkgrey
+        commentTextField.contentVerticalAlignment = .top
+        commentTextField.textColor = .charcoal
+        commentTextField.backgroundColor = .lightgrey1
+        commentLengthLabel.textColor = .darkgrey
+        
+    }
+    
+    private func setNavBar() {
+        self.navigationItem.title = "독서 일지"
+        self.navigationController?.navigationBar.topItem?.title = ""
+        let rightButton = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(postDiary(sender:)))
+        self.navigationItem.rightBarButtonItem = rightButton
+        self.navigationItem.rightBarButtonItem?.tintColor = .main
+        self.navigationController?.navigationBar.tintColor = .darkgrey
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+}
+
+
+
+extension DaillyReadingWritenViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            DispatchQueue.main.async {
+                self.reviewImageHeight.constant = 75
+                self.pickedImage = image
+                self.reviewImagePopButton.isHidden = false
+                self.reviewImageView.image = image
+            }
+         }
+        dismiss(animated: true, completion: nil)
+     }
+}
+
+extension DaillyReadingWritenViewController {
+    func openLibrary(){
+      picker.sourceType = .photoLibrary
+      present(picker, animated: false, completion: nil)
+    }
+
+    func openCamera(){
+        if(UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+            }else{
+            print("LOG - 카메라 로드 실패")
+        }
+    }
+    
+    @objc func open() {
+        addImageAlertAction()
+    }
+    
+    func addImageAlertAction() {
+        let alert =  UIAlertController(title: "이미지 선택", message: "일지에 첨부할 이미지를 선택해주세요.", preferredStyle: .actionSheet)
+        let library =  UIAlertAction(title: "갤러리 선택", style: .default) { (action) in
+            self.openLibrary()
+        }
+        let camera =  UIAlertAction(title: "사진 촬영", style: .default) { (action) in
+            self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
     }
 }
