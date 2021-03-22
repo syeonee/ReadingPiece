7c408abe6b964b55bb0d19e3b665602ec1f959eb
aawlgus <78068968+jihyundev@users.noreply.github.com>
2021-03-22 17:05:19 +0900

    feat: 애플 로그인 관련 identifier 등록, 코드 연결
    소프트스퀘어드 팀 등록, 번들 ID를 수정하였습니다. 애플 로그인 관련 signing capability를 추가하였고 로그인 request와 성공 후 데이터를 받아오는 코드를 생성하였습니다.

--- ReadingPiece/Configuration/Constants.swift
+++ ReadingPiece/Configuration/Constants.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 struct Constants {
-    let DEV_BASE_URL = "https://dev.maekuswant.shop/"
+    static let DEV_BASE_URL = "https://dev.maekuswant.shop/"
+    static let BASE_URL = "https://prod.maekuswant.shop/"
+    
     let USERDEFAULT_KEY_CURRENT_TIMER_TIME = "savedTimeInt"
     // jwtToken은 추후 userDefualt에 저장한 값을 불러오는 것으로 수정 필요
     let testAccessTokenHeader: [String : String] = ["Content-Type": "application/json", "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksImlhdCI6MTYxNjExODA1MCwiZXhwIjoxNjQ3NjU0MDUwLCJzdWIiOiJ1c2VySW5mbyJ9.3MUlZisCd5vqohXDYzwt74zNvzGUnZrTrGtSQXCPlxY"]
---
+++ ReadingPiece/ViewController/Login/NetworkManager/AppleLoginNetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  AppleLoginNetworkManager.swift
+//  ReadingPiece
+//
+//  Created by 정지현 on 2021/03/22.
+//
+
+import Foundation
+import AuthenticationServices
+
+class AppleLoginNetworkManager {
+    
+}
