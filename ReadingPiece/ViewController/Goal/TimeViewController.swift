80cb872d481b79937398f712029390dc7bf0a2d2
RobertHan96 <yshan4329@gmail.com>
2021-03-23 02:39:21 +0900

    feat: 목표 추가 화면 버튼 색상 변경 로직, API 연동

--- ReadingPiece/ViewController/Goal/TimeViewController.swift
+++ ReadingPiece/ViewController/Goal/TimeViewController.swift
@@ -11,32 +11,69 @@ class TimeViewController: UIViewController {
     // Term VC에서 옵셔널 검사를 하고, 값을 넘겨줄 것 이기 때문에 편의상 논-옵셔널 변수로 생성
     var time: Int = 0
     var period: String = ""
+    var amount: Int = 0
     
+    @IBOutlet weak var readingTimeLabel: UILabel!
     @IBOutlet weak var timeTextField: UITextField!
     @IBOutlet weak var timeSelectButton: UIButton!
     @IBOutlet weak var arrowButton: UIButton!
     @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var addGoalButton: UIButton!
     
     let datePicker = UIDatePicker()
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         createDatePicker()
     }
     
-    // Delegate 패턴으로 전달받은 Goal 값을 이용해 목표 설정
+    @objc func donePressed() {
+        let minute = Int(datePicker.countDownDuration/60)
+        time = minute
+        timeTextField.text = "\(minute)"
+        minuteLabel.isHidden = false
+        readingTimeLabel.text = """
+                                    매일  \(minute) 분
+                                    책을 읽을 거에요
+                                """
+        addGoalButton.setImage(UIImage(named: "selectedNext"), for: .normal)
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func timeSelectButtonTapped(_ sender: Any) {
+        timeTextField.becomeFirstResponder()
+    }
+    
+    @IBAction func addGoalAction(_ sender: UIButton) {
+        setReadingGoal()
+    }
+    
+    func initTerm(readingPeriod: String, readingAmount: Int) {
+        period = readingPeriod
+        amount = readingAmount
+    }
+    
+    // 전달받은 Goal 값을 이용해 목표 설정
     func setReadingGoal() {
-        let req = ReadingGoalRequest(Goal(period: "D", amount: 1, time: 1))
+        if amount != 0 && period != "" && time != 0 {
+            postUserReadingGoal()
+        }
+    }
+    
+    func postUserReadingGoal() {
+        print("LOG - 목표설정 완료", amount, period, time)
+        let req = ReadingGoalRequest(Goal(period: period, amount: amount, time: time))
                                 
         _ = Network.request(req: req) { (result) in
                 
                 switch result {
                 case .success(let userResponse):
-                    print(userResponse)
+                    guard let searchVC = UIStoryboard(name: "Goal", bundle: nil).instantiateViewController(withIdentifier: "searchBookViewController") as? SearchBookViewController else { return }
+                    self.navigationController?.pushViewController(searchVC, animated: true)
                 case .cancel(let cancelError):
                     print(cancelError!)
                 case .failure(let error):
-                    print(error!)
+                    self.presentAlert(title: "서버와의 연결이 원활하지 않습니다.", isCancelActionIncluded: false)
             }
         }
     }
@@ -63,15 +100,4 @@ class TimeViewController: UIViewController {
         datePicker.datePickerMode = .countDownTimer
         
     }
-    
-    @objc func donePressed() {
-        timeTextField.text = "\(Int(datePicker.countDownDuration/60))"
-        minuteLabel.isHidden = false
-        self.view.endEditing(true)
-    }
-    
-    @IBAction func timeSelectButtonTapped(_ sender: Any) {
-        timeTextField.becomeFirstResponder()
-    }
-    
 }
