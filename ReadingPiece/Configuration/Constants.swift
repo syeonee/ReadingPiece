7de048e5f2049f3abfce8e74bccdce1d201f266b
RobertHan96 <yshan4329@gmail.com>
2021-03-30 20:27:47 +0900

    fix: 일지% 입력 화면 오토레이아웃

--- ReadingPiece/Configuration/Constants.swift
+++ ReadingPiece/Configuration/Constants.swift
@@ -10,7 +10,7 @@ import KeychainSwift
 
 struct Constants {
     static let KEYCHAIN_TOKEN = KeychainSwift(keyPrefix: Keys.keyPrefix).get(Keys.token)
-    static let DEV_BASE_URL = "https://dev.maekuswant.shop/"
+    static let DEV_BASE_URL = "https://prod.maekuswant.shop/"
     static let BASE_URL = "https://prod.maekuswant.shop/"
     let keychain = KeychainSwift(keyPrefix: Keys.keyPrefix)
     static let USERDEFAULT_KEY_CURRENT_TIMER_TIME = "savedTimeInt"
